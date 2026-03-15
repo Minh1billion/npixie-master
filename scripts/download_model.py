146f872c4c74ab5632ce80fe89f5ca94dc296435
Minh1billion <minh01212929979@gmail.com>
2026-03-15 20:20:13 +0700

    fix(scripts): adjust download_model file

--- scripts/download_model.py
+++ scripts/download_model.py
@@ -4,8 +4,8 @@ from huggingface_hub import hf_hub_download
 
 load_dotenv()
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-SAVE_DIR = os.path.join(BASE_DIR, "models")
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+SAVE_DIR = os.path.join(BASE_DIR, "specxel", "models")
 HF_TOKEN = os.getenv("HF_TOKEN")
 HF_REPO_ID = os.getenv("HF_REPO_ID")
 
@@ -15,3 +15,14 @@ hf_hub_download(
     local_dir=SAVE_DIR,
     token=HF_TOKEN
 )
+
+for f in os.listdir(SAVE_DIR):
+    if not f.endswith(".pth"):
+        path = os.path.join(SAVE_DIR, f)
+        if os.path.isfile(path):
+            os.remove(path)
+
+cache_dir = os.path.join(SAVE_DIR, ".cache")
+if os.path.isdir(cache_dir):
+    import shutil
+    shutil.rmtree(cache_dir)
